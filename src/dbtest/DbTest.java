fa0ff09689f1f9aa933f8e0ed8a7e09d599ad2b2
Andrey Golubev <AndreyGolubev85@gmail.com>
2018-01-27 09:31:02 +1100

    2

---
+++ src/dbtest/DbTest.java
@@ -0,0 +1,46 @@
+package dbtest;
+
+import java.sql.*;
+
+public class DbTest {
+    // JDBC URL, username and password of MySQL server
+    private static final String url = "jdbc:mysql://localhost:3306/footbalPlayers";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    // JDBC variables for opening and managing connection
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
+
+    public static void main(String args[]) {
+        String query = "INSERT INTO users(username,game,score) VALUES ('dima2','3', 5);";
+
+        try {
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(url, user, password);
+
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.execute();
+
+            // getting Statement object to execute query
+            //stmt = con.createStatement();
+
+            // executing SELECT query
+            //rs = stmt.executeQuery(query);
+
+            /*while (rs.next()) {
+
+                System.out.println(rs.getString(1));
+            }*/
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+    }
+}
